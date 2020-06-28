d7dd4ebd7eb78d9059b9d77d45b8e25bc11f22db
Lars Karbo <mail@larskarbo.no>
2020-06-28 21:15:13 +0200

    clean up and use ServerChart type

--- script.js
+++ script.js
@@ -1,56 +1,65 @@
-var Ajv = require('ajv')
+var Ajv = require("ajv");
 
-const schema = require('./Structure.json')
+const schema = require("./ServerChart.json");
 
+const ajv = new Ajv();
 
-const ajv = new Ajv()
+var LineByLineReader = require("line-by-line"),
+  lr = new LineByLineReader("here.json");
 
-
-var LineByLineReader = require('line-by-line'),
-    lr = new LineByLineReader('here.json');
-
-lr.on('error', function (err) {
-	console.log('err: ', err);
-	// 'err' contains error object
+lr.on("error", function (err) {
+  console.log("err: ", err);
+  // 'err' contains error object
 });
 
-let i = 0
-lr.on('line', function (line) {
-	// 'line' contains the current line without the trailing newline character.
-	console.log('line: ', ++i);
-	const chart = JSON.parse(line)
-	// delete chart._id
-	if(!chart.chartData){
-		console.log('skipping')
-		return
+let i = 0;
+lr.on("line", function (line) {
+  // 'line' contains the current line without the trailing newline character.
+  // console.log("line: ", ++i);
+  const chart = JSON.parse(line);
+  // delete chart._id
+  if (!chart.chartData) {
+    console.log("skipping");
+    return;
 	}
-	chart.chartData.elements = chart.chartData.elements.map(e => ({
-		start: e.start * 1,
-		end: e.end * 1,
-		color: e.color,
-		text: e.text,
-		lane: e.lane,
-		color: e.color,
-	}))
-	chart.data = chart.chartData
-	delete chart.chartData
-	delete chart._id
-	delete chart.chartid
-	delete chart._updated_at
-	delete chart._created_at
-	delete chart.data.id
-	delete chart.data.metaInfo
-	delete chart.metaInfo
-	var valid = ajv.validate(schema, chart)
-	if (!valid) {
-		console.log( chart)
-		console.log(ajv.errors)
-		throw new Error()
+
+  if (chart.chartData.elements == 0) {
+    console.log("skipping e");
+    return;
+  }
+
+
+	const chartid = chart.chartid
+  const dataForServer = {
+    title: chart.title || "",
+    description: chart.description || "",
+    elements: chart.chartData.elements.map((e) => ({
+      start: e.start * 1,
+      end: e.end * 1,
+      color: e.color,
+      text: e.text,
+      lane: e.lane,
+    })),
+    colorTags: chart.chartData.colorTags || [],
+    shape: chart.chartData.shape,
+    lanes: chart.chartData.lanes,
+    lanesConfig: chart.chartData.lanesConfig,
+  };
+
+  var valid = ajv.validate(schema, dataForServer);
+  if (!valid) {
+		console.log("error")
 		return
-	}
+    console.log(dataForServer);
+    console.log(ajv.errors);
+    throw new Error();
+    return;
+  }
+
+  // upload to firebase here
 });
 
-lr.on('end', function () {
-	// All lines are read, file is closed now.
-	console.log('done')
+lr.on("end", function () {
+  // All lines are read, file is closed now.
+  console.log("done");
 });
