ead50390b5fbd1c68ef130f053e57722b97c1682
Lars Karbo <mail@larskarbo.no>
2020-08-01 17:30:52 +0200

    go

---
+++ README.md
@@ -0,0 +1,10 @@
+how to:
+
+
+use this: /usr/local/bin/mongoexport --uri="mongodb://user2f6769:e69729wUXcPLw28643e@cluster-pgrs1002-0-us-east-1-scalabledbs.cloudstrap.io:29002,cluster-pgrs1002-1-us-east-1-scalabledbs.cloudstrap.io:29002,cluster-pgrs1002-2-us-east-1-scalabledbs.cloudstrap.io:29002/pg-app-2-us-57gagyy9xq3pta5kvgpzs2dh6gv7w5?replicaSet=pgrs1002&ssl=true"  --ssl --collection Chart --type json --out ./here2.json
+
+then:
+
+```
+node script.js
+```
--- script.js
+++ script.js
@@ -1,65 +1,120 @@
 var Ajv = require("ajv");
-
+const fs = require("fs-extra")
 const schema = require("./ServerChart.json");
 
 const ajv = new Ajv();
 
+var firebase = require("firebase/app");
+require("firebase/firestore");
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDZIH0Vogv07ZWCUMwPn1gaBaF_6rAP_zg",
+	authDomain: "napchart-1abe4.firebaseapp.com",
+	databaseURL: "https://napchart-1abe4.firebaseio.com",
+	projectId: "napchart-1abe4",
+	storageBucket: "napchart-1abe4.appspot.com",
+	messagingSenderId: "747326670843",
+	appId: "1:747326670843:web:39891acdbdf5df1cd8ed5e",
+	measurementId: "G-NP62410MLV",
+};
+firebase.initializeApp(firebaseConfig);
+
 var LineByLineReader = require("line-by-line"),
-  lr = new LineByLineReader("here.json");
+	lr = new LineByLineReader("here2.json");
 
 lr.on("error", function (err) {
-  console.log("err: ", err);
-  // 'err' contains error object
+	console.log("err: ", err);
+	// 'err' contains error object
 });
 
 let i = 0;
-lr.on("line", function (line) {
-  // 'line' contains the current line without the trailing newline character.
-  // console.log("line: ", ++i);
-  const chart = JSON.parse(line);
-  // delete chart._id
-  if (!chart.chartData) {
-    console.log("skipping");
-    return;
+let errors = 0
+var db = firebase.firestore();
+const already = fs.readFileSync("./already.txt", { encoding: "utf8" }).split("\n")
+console.log('already: ', already);
+let added = already.length
+
+lr.on("line", async function (line) {
+	lr.pause();
+	// 'line' contains the current line without the trailing newline character.
+	// console.log("line: ", ++i);
+	const chart = JSON.parse(line);
+	// delete chart._id
+	if (!chart.chartData) {
+		console.log("skipping");
+		return;
 	}
 
-  if (chart.chartData.elements == 0) {
-    console.log("skipping e");
-    return;
-  }
+	if (chart.chartData.elements == 0) {
+		// console.log("skipping e");
+		// return;
+	}
 
 
 	const chartid = chart.chartid
-  const dataForServer = {
-    title: chart.title || "",
-    description: chart.description || "",
-    elements: chart.chartData.elements.map((e) => ({
-      start: e.start * 1,
-      end: e.end * 1,
-      color: e.color,
-      text: e.text,
-      lane: e.lane,
-    })),
-    colorTags: chart.chartData.colorTags || [],
-    shape: chart.chartData.shape,
-    lanes: chart.chartData.lanes,
-    lanesConfig: chart.chartData.lanesConfig,
-  };
+	const dataForServer = {
+		title: chart.title || "",
+		description: chart.description || "",
+		elements: chart.chartData.elements.map((e) => ({
+			start: e.start * 1,
+			end: e.end * 1,
+			color: e.color,
+			text: e.text,
+			lane: e.lane,
+		})),
+		colorTags: chart.chartData.colorTags || [],
+		shape: chart.chartData.shape,
+		lanes: chart.chartData.lanes,
+		lanesConfig: chart.chartData.lanesConfig || {},
+	};
 
-  var valid = ajv.validate(schema, dataForServer);
-  if (!valid) {
-		console.log("error")
+	var valid = ajv.validate(schema, dataForServer);
+	if (!valid) {
+		errors++
+		console.log("some error")
+		// console.log(ajv.errors);
+		// console.log(dataForServer);
+		lr.resume();
 		return
-    console.log(dataForServer);
-    console.log(ajv.errors);
-    throw new Error();
-    return;
-  }
+	}
 
-  // upload to firebase here
+	// const idAlreadyAdded = async (id) => {
+	// 	return db
+	// 		.collection('charts')
+	// 		.doc(id)
+	// 		.get()
+	// 		.then((doc) => {
+	// 			return doc.exists
+	// 		})
+	// }
+
+	const idAlreadyAdded = async (id) => {
+		return already.includes(id)
+	}
+
+	if (await idAlreadyAdded(chartid)) {
+		console.log('already added ', chartid)
+		lr.resume();
+		return
+	}
+	console.log(chartid)
+	await db
+		.collection('charts')
+		.doc(chartid)
+		.set(dataForServer)
+		.then((docRef) => {
+
+			added++
+			console.log('added chartid: ', chartid, "number: ", added, "errors: ", errors);
+			fs.appendFile("./already.txt", "\n" + chartid)
+			lr.resume();
+			return chartid
+		})
+
+	return
 });
 
 lr.on("end", function () {
-  // All lines are read, file is closed now.
-  console.log("done");
+	// All lines are read, file is closed now.
+	console.log("done", errors, "errors");
 });
